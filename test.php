d7e5fd9911706cfe10958d6a7cb3cf82c1d8e6ab
TonyDaGroot <tony.nguyen@sendviper.io>
2019-03-08 23:09:39 -0600

    Update test.php

--- test.php
+++ test.php
@@ -8,17 +8,17 @@
 
 <body>
   <div class="container">
-      <form action="" class="form-horizontal"  role="form">
+      <form action="result.php" class="form-horizontal"  role="form">
           <fieldset>
               <legend>Test</legend>
               <div class="form-group">
                   <label for="dtp_input1" class="col-md-2 control-label">DateTime Picking</label>
                   <div class="input-group date form_datetime col-md-5" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="dtp_input1">
-                      <input class="form-control" size="16" type="text" value="" readonly>
+                      <input class="form-control" size="16" name="datetime" type="text" value="" readonly>
                       <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
   					<span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
                   </div>
-  				<input type="hidden" id="dtp_input1" value="" /><br/>
+  				<input type="submit" value="Submit"  /><br/>
               </div>
 
 
